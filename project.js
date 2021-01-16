93cc1dec601a2d57cfa82afce66fdeca7828d322
dylan-hatch <76062513+dylan-hatch@users.noreply.github.com>
2021-01-16 11:35:19 -0500

    Create project.js

---
+++ project.js
@@ -0,0 +1,11 @@
+// set up canvas with size
+function setup() {
+  createCanvas(400, 400);
+}
+
+// Hold A Ball, basically
+function draw() {
+  background(0);
+  fill(255);
+  ellipse(mouseX, mouseY, 80, 80);
+}
